0d3f4a4bdadb9afa351f1c59eafa57f677f069e8
Tassia Shibuya <tassia.shibuya@gmail.com>
2020-04-03 23:07:18 -0700

    made some changes in the html and js files

--- script.js
+++ script.js
@@ -1,92 +1,58 @@
-// // create the global variables
-let dayHours = [];
-let hour;
+// Global variables
+const arrayHours = [];
+const index = 0;
+const dateHour = $('data-time').val();
+const $inputTask = $('.description').text();
 
-let dayTask = {};
+console.log(dateHour);
+console.log($inputTask);
 
-// let moment = moment().hour();
+// open the browser see date and time on the top
 
-// Display the current date at the top of the calendar
 function currentDay() {
     let currentDate = moment().format("LLLL");
     $("#currentDay").html(currentDate);
 }
 currentDay()
 
-// Display timeblocks for standard business hours, input space and save button
-
-function timeBlocls(){
-
-for (let hour = 9; hour < 18; hour++) {
-    dayHours.push(hour);
-    
-    console.log(hour);
-
-    // $(".dayHour").text(hour + "am") {
-        if(hour <= 12){
-            $(".dayHour").text(hour + " am");
-
-        } else {
-            $(".dayHour").text(hour + " pm");
-        }
-    }
-    // adding the timeblocks in the JS file
-    // let $div = $("<div>").addClass('input-group time-block row ').attr('data-time', hour);
-    // $div.text(hour);
-    // $(".container").append($div);
-    
-    // let $textArea = $("<textarea>").addClass(`form-control text-area description`);
-    // $div.append($textArea);
-
-    // let $saveBtn = $("<button>").addClass(`btn btn-outline-secondary saveBtn`);
-    // $div.append($saveBtn);
-}
-
-function currentTime() {
-        let currentTime = moment().hour();
-        scheduleHour = parseInt(hour);
-        
-    if(scheduleHour === currentTime){
-        $(this).find('textarea').addClass('present');
-
-        console.log("Hello");
-    }else if (scheduleHour < currentTime) {
-        $(this).find('textarea').addClass('past');
-        
-        console.log("bye");
-    } else {
-        $(this).find('textarea').addClass('future');
-        console.log("pause")
-    }
-        console.log(currentTime);
-}
-
-currentTime()
-
-timeBlocls();
-
+// time at the page refreshes clock every 10 seconds
 setInterval(currentDay, 1000);
 
-// when the user click on the save button for that time block the text is saved in local storage (event listener)
-$('.saveBtn').on("click", function(e){
+// the text input bar is colored based in indication whether it is past, present and future
+$.each($('.time-block'), function() {
+
+    let currentTime = moment().format();
+
+if(dateHour === currentTime){
+    $(this).find('textarea').addClass('present');
+}else if (dateHour < currentTime) {
+    $(this).find('textarea').addClass('past');
+} else {
+    $(this).find('textarea').addClass('future');
+}
+}) 
+
+
+$("button").on('click', function(e) {
     e.preventDefault();
-    // eventlistener to capture what the user typed
-    
-    const inputText = $('.description').val().trim();
-    console.log(inputText);
-    if(inputText === ""){
-        return;
+
+    let timeTask = parseInt(dateHour);
+
+    let taskLog = {
+        time: timeTask,
+        todo: $inputTask
     }
 
-    storeInput()
+console.log(taskLog);
+
 })
 
 
-// store what the user stored in local sotarge (key: hour value: user input); (for form=use .val - 
-function storeInput(){
-    localStorage.setItem('scheduleInput', JASON.stringify(dayTask));
-}
 
-// create a function to change the color of the input space according to the time of the day: past, present or future
+// see the schedule with input field and save button for each line by hour (9am to 5pm)
+// input text in the text field
+// if
+// push the button save 
+    // info get stored into local storage
 
-// when user refresh the page the saved event
+// refresh the page and info still there
